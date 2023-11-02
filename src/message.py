57a5198cd3a0303d41791acd2b515404ebe29af4
Gabriela Cunha <73352341+gabrielasfc@users.noreply.github.com>
2023-11-02 23:47:21 +0000

    subscriptions and logger

--- src/message.py
+++ src/message.py
@@ -3,39 +3,31 @@ import io
 from datetime import datetime
 
 class Message:
-    def __init__(self, msg_type, flags=None, timestamp=0, nr_neighbours=0, nr_interfaces=0, nr_jumps=0, neighbours=None, interfaces=None, jumps=None):
+    def __init__(self, msg_type, flags=None, timestamp=0, neighbours=None, jumps=None):
         # Header
         self.type = msg_type
         self.flags = flags
         self.timestamp = timestamp
-        self.nr_neighbours = nr_neighbours
-        self.nr_interfaces = nr_interfaces
-        self.nr_jumps = nr_jumps
         # Data
         self.neighbours = neighbours
-        self.interfaces = interfaces
         self.jumps = jumps
     
     def __str__(self):
-        return f"type: {self.type}"
+        return f"Type: {self.type} | Timestamp: {self.timestamp} | Neighbours: {self.neighbours} | Jumps: {self.jumps}"
 
     def __repr__(self):
-        return f"type: {self.type}"
+        return f"Type: {self.type} | Timestamp: {self.timestamp} | Neighbours: {self.neighbours} | Jumps: {self.jumps}"
 
     def serialize(self):
         # Header
         dictionary = { 
             "type": self.type, 
-            "timestamp": self.timestamp,
-            "nr_neighbours": self.nr_neighbours,
-            "nr_interfaces": self.nr_interfaces,
-            "nr_jumps": self.nr_jumps
+            "timestamp": self.timestamp
         }
 
         # Data
         if self.neighbours is not None:
             dictionary["neighbours"] = self.neighbours
-            dictionary["interfaces"] = self.interfaces
 
         if self.jumps is not None:
             dictionary["jumps"] = self.jumps
@@ -46,15 +38,10 @@ class Message:
     def deserialize(bytes):
         dictionary = pickle.loads(bytes)
 
-        message = Message(dictionary["type"], 
-                    timestamp=dictionary["timestamp"],
-                    nr_neighbours=dictionary["nr_neighbours"],
-                    nr_interfaces=dictionary["nr_interfaces"],
-                    nr_jumps=dictionary["nr_jumps"])
+        message = Message(dictionary["type"], timestamp=dictionary["timestamp"])
 
         if "neighbours" in dictionary:
             message.neighbours = dictionary["neighbours"]
-            message.interfaces = dictionary["interfaces"]
         
         elif "jumps" in dictionary:
             message.jumps = dictionary["jumps"]
--- src/node.py
+++ src/node.py
@@ -2,6 +2,7 @@ import json
 import sys
 import socket
 import threading
+import logging
 from message import Message
 from datetime import datetime
 
@@ -9,7 +10,6 @@ class Node:
     def __init__(self, ntype, bootstrapper_addr=None, file=None):
         self.type = ntype
         self.tree = dict()
-        self.interfaces = list()
         self.neighbours = list()
         self.control_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,61 +22,75 @@ class Node:
         else:
             addr_list = bootstrapper_addr.split(":")
             self.bootstrapper_addr = (addr_list[0], int(addr_list[1]))
+        
+        logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
+        self.logger = logging.getLogger()
+        self.logger.info("Control service listening on port 7777 and streaming service on port 7778")
 
 
     def request_neighbours(self):
         self.control_socket.sendto(Message(0).serialize(), self.bootstrapper_addr)
+        self.logger.info("Control Service: Asked for neighbours")
 
-        msg, addr = self.control_socket.recvfrom(1024)
+        msg, addr = self.control_socket.recvfrom(1024)  
         decoded_msg = Message.deserialize(msg)
 
         if decoded_msg.type == 1:
             self.neighbours = decoded_msg.neighbours
-            self.interfaces = decoded_msg.interfaces
-
-        print(self.neighbours)
+            self.logger.info("Control Service: Neighbours received")
+            self.logger.debug(f"Neighbours: {self.neighbours}")
 
 
     def neighbours_worker(self, addr):
         for key, value in self.nodes.items():
             if addr[0] in value["interfaces"]: # é esse o servidor
-                msg = Message(1, nr_neighbours=len(value["neighbours"]), nr_interfaces=len(value["interfaces"]), neighbours=value["neighbours"], interfaces=value["interfaces"])
+                msg = Message(1, neighbours=value["neighbours"])
                 self.control_socket.sendto(msg.serialize(), addr)
+                self.logger.info(f"Control Service: Neighbours sent to {addr[0]}")
+                self.logger.debug(f"Neighbours: {msg}")
             
 
     def subscription_worker(self, addr, msg):
+        if msg.jumps is None:
+            msg.jumps = list()
+        msg.jumps.append(addr[0])
+
+        timestamp = float(datetime.now().timestamp())
+        diff = timestamp - msg.timestamp
+
+        if msg.jumps[0] in self.tree:
+            latency, neighbour = self.tree[msg.jumps[0]]
+            
+            if diff < latency:
+                self.logger.debug(f"Control Service: Changing from neighbour {self.tree[msg.jumps[0]][1]} to neighbour {addr[0]}")
+                self.tree[msg.jumps[0]] = (diff, addr[0])
+
+        else:
+            self.logger.debug(f"Control Service: Adding neighbour {addr[0]}")
+            self.tree[msg.jumps[0]] = (diff, addr[0])
+
         if self.type != 2:
-            for ind, neighbour in enumerate(self.neighbours):
+            for neighbour in self.neighbours:
                 if neighbour != addr[0]:
-                    if msg.jumps is None:
-                        msg.jumps = list()
-                    msg = msg.jumps.append(self.interfaces[ind])
                     self.control_socket.sendto(msg.serialize(), (neighbour, 7777))
-        else: # RP Node
-            timestamp = datetime.timestamp(datetime.now())
-            initial_timestamp = datetime.strptime(msg.timestamp, '%Y-%m-%d %H:%M:%S')
-            time_differences_s = timestamp.seconds - initial_timestamp.seconds
-            time_differences_ms = time_differences_s * 1000
-
-            if msg.jumps[0] in self.tree:
-                latency, neighbour = self.tree[msg.jumps[0]]
-
-                if time_differences_ms < latency:
-                    self.tree[msg.jumps[0]] = (time_differences_ms, msg.jumps[-1])
-            else:
-                self.tree[msg.jumps[0]] = (time_differences_ms, msg.jumps[-1])
-
+                    self.logger.info(f"Control Service: Subscription message sent to neighbour {neighbour}")
+                    self.logger.debug(f"Message sent: {msg}")
     
+
     def control_service(self):
-        # Nó folha
-        if len(self.neighbours) == 1:
-            timestamp = int(datetime.now().timestamp())
-            self.control_socket.sendto(Message(2, timestamp=timestamp).serialize(), (self.neighbours[0], 7777))
+        if len(self.neighbours) == 1: # Nó folha
+            timestamp = float(datetime.now().timestamp())
+            msg = Message(2, timestamp=timestamp)
+            self.control_socket.sendto(msg.serialize(), (self.neighbours[0], 7777))
+            self.logger.info(f"Control Service: Subscription message sent to neighbour {self.neighbours[0]}")
+            self.logger.debug(f"Message sent: {msg}")
 
         try:
             while True:
                 msg, addr = self.control_socket.recvfrom(1024)
                 decoded_msg = Message.deserialize(msg)
+                self.logger.info(f"Control Service: Subscription message received from neighbour {addr[0]}")
+                self.logger.debug(f"Message received: {decoded_msg}")
 
                 if self.type == 0 and decoded_msg.type == 0:
                     threading.Thread(target=self.neighbours_worker, args=(addr,)).start()
@@ -86,3 +100,4 @@ class Node:
             
         finally:
             self.control_socket.close()
+
